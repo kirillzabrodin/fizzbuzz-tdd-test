a1e25c825a3246a4ebe9cd43e16f52a9f23d9e48
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-01 18:46:54 +0000

    first test

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,2 +1,16 @@
-Hierarchy
-User story > feature > unit test
+'use strict'
+
+describe('FizzBuzz', () => {
+
+    var game
+
+    beforeEach(() => {
+        game = new FizzBuzz()
+    })
+
+    it('Numbers returns Fizz if they are divisible by 3', () => {
+        expect(game.play(3)).toBe('Fizz')
+        expect(game.play(12)).toBe('Fizz')
+        expect(game.play(3 * 69)).toBe('Fizz')
+    })
+})
